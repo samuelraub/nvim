e0b8653fb91009f5ab29884b0060f5438d0acd6e
Samuel Raub <samuel.raub@gmail.com>
2025-02-27 16:37:02 +0100

    better multicursor, mini jump

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,5 +4,19 @@ return {
   config = function()
     require("mini.operators").setup()
     require("mini.ai").setup()
+    require("mini.jump").setup()
+
+    local jump2d = require("mini.jump2d")
+    local jump_line_start = jump2d.builtin_opts.line_start
+    jump2d.setup({
+      labels = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      spotter = jump_line_start.spotter,
+      hooks = { after_jump = jump_line_start.hooks.after_jump },
+      silent = true,
+      allowed_lines = {
+        blank = false,
+        cursor_at = false,
+      },
+    })
   end,
 }
